6896e1a8e2b2c2da86454b5160c1ef5186fde59d
JayLeetwan <jakub.litkowski@gmail.com>
2023-08-19 19:30:36 +0200

    if else

--- 05-objects.js
+++ 05-objects.js
@@ -1,10 +1,28 @@
 const cookie = {
     name: "czekoladowe ciastko",
     isGlutenFree: false,
-    "+smaczne": true
+    // "+smaczne": true
+    eatCookie: function () {
+        console.log("I am eating "+ this.name);
+    }
 }
 
 cookie.name = "piegusek";
 cookie.isGlutenFree = true;
-console.log(cookie["+smaczne"]);
-console.log(cookie);
+// console.log(cookie["+smaczne"]);
+// console.log(cookie);
+// cookie.eatCookie();
+
+class Cookie {
+ constructor(name, isGlutenFree){
+    this.name = name;
+    this.isGlutenFree = isGlutenFree;
+ }   
+ eatCookie(){
+    console.log("I am eating "+ this.name);
+}
+}
+const myCookie = new Cookie("Iced Oatmeal", false);
+
+// console.log(myCookie);
+myCookie.eatCookie();
